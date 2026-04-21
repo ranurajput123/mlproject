b058c97e328ac9571c1a90401bed7aba603255cd
ranurajput123 <ranurajput112@gmail.com>
2026-04-21 21:41:03 +0530

    exceptions

--- src/exception.py
+++ src/exception.py
@@ -1,11 +1,12 @@
 import sys
+import logging
 
 ### how youe message should look like while exception error
-def error_message_detail(error, error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()  # on which line and file exception has occured
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occured in python script name [{0}] linr number [{1}] error message [{2}]".format()
-    file_name, exc_tb.tb_lineno,str(error)
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+     file_name,exc_tb.tb_lineno,str(error))
 
     return error_message
 
@@ -13,9 +14,12 @@ def error_message_detail(error, error_detail:sys):
 
 class CustomException(Exception):
     def __init__(self, error_message, error_details:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message=error_message_detail(error_message, error_detail=error_details)
 
     ### printing error message for exception
     def __str__(self):
         return self.error_message
+
+
+    
